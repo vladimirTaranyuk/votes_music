65df7fe47d28aa4ee075000dc06104632d5ea12a
MacGilla <fire.ball211104@gmail.com>
2025-07-08 00:53:32 +0700

    add voters

--- server.js
+++ server.js
@@ -11,56 +11,65 @@ const io = socketIo(server);
 let votes = {
     left: 0,
     right: 0,
-    voters: [], // Теперь это массив объектов
+    voters: [], // Массив объектов {socketId, username, side}
     randomResult: null
 };
 
 app.use(express.static(path.join(__dirname, '/public')));
-// В server.js убедитесь, что есть обработчик для корневого пути
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/Index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 io.on('connection', (socket) => {
     // Отправляем текущее состояние при подключении
     socket.emit('update', votes);
-    
+
+    // Обработка регистрации пользователя
+    socket.on('register', (username) => {
+        // Сохраняем имя пользователя в объекте socket
+        socket.username = username;
+    });
+
     // Обработка голосования
-    socket.on('vote', (side) => {
+    socket.on('vote', (data) => {
+        const { side, username } = data;
         const voterId = socket.id;
-        
+
         // Проверяем, не голосовал ли уже этот пользователь
-        const alreadyVoted = votes.voters.some(voter => voter.id === voterId);
-        
-        if (!alreadyVoted) {
+        const alreadyVoted = votes.voters.some(voter => voter.socketId === voterId);
+
+        if (!alreadyVoted && username) {
             votes[side]++;
             votes.voters.push({
-                id: voterId,
+                socketId: voterId,
+                username: username,
                 side: side
             });
             io.emit('update', votes);
         }
     });
-    
+
     // Обработка сброса
     socket.on('reset', () => {
         votes.left = 0;
         votes.right = 0;
-        votes.voters = []; // Очищаем список проголосовавших
+        votes.voters = [];
         votes.randomResult = null;
         io.emit('update', votes);
-        io.emit('resetVotingState'); // Сообщаем клиентам о сбросе
+        io.emit('resetVotingState');
     });
-    
+
     // Обработка рандомайзера
     socket.on('randomize', () => {
         votes.randomResult = Math.random() < 0.5 ? 'left' : 'right';
         io.emit('update', votes);
     });
-});
 
-app.get('/', (req, res) => {
-   res.send('Главная страница работает!');
+    // При отключении пользователя
+    socket.on('disconnect', () => {
+        // Можно добавить логику обработки отключения, если нужно
+    });
 });
 
 const PORT = process.env.PORT || 8080;
